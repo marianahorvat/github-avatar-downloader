817cde704cd991cfce33c5bfd9e741fc8dc343e2
Mariana <mariana.horvat.qa@gmail.com>
2019-04-09 01:16:49 +0000

    Print each avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -1,13 +1,38 @@
 var request = require('request');
+var secret = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 
+// function getRepoContributors(repoOwner, repoName, cb) {
+//   var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+//   request(url, function(err, res, body) {
+//     cb(err, body);
+//   });
+// }
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    var ObjData = JSON.parse(body)
+
+    cb(err, ObjData);
+    ObjData.forEach(function(elem) {
+      console.log(elem.avatar_url);
+    });
+  });
 }
+
+
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
